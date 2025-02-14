28572fa033a40c3a76f0e93117e86ec93d054fe6
Ferrante Enriques <ferrante.enriques@gmail.com>
2025-02-14 18:36:33 +0100

    main and v 2.0.4

---
+++ astrodom/__main__.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt6.QtWidgets import QApplication
+from  ad import MainWindow
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()

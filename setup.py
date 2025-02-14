ce7bacfcc2880b6582bdc391448250b06bca8d24
Ferrante Enriques <ferrante.enriques@gmail.com>
2025-02-14 17:59:48 +0100

    commit to master

---
+++ setup.py
@@ -0,0 +1,69 @@
+############################################################
+# -*- coding: utf-8 -*-
+#
+#  Astroimaging catalogue software
+#
+# Ferrante Enriques
+# (c) 2020
+#
+# License MIT
+#
+###########################################################
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            # __version__ = "0.9"
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+long_description = read("README.md")
+
+setup(
+    name="astrodom",
+    version=get_version("astrodom/__init__.py"),
+    packages=find_packages(),
+    python_requires=">=3.12",
+    install_requires=[
+        "astropy",
+        "matplotlib",
+        "PyQt5",
+        "pyqtgraph",
+        "numpy",
+        "pandas"
+    ],
+    include_package_data=True,
+    url="https://github.com/fenriques/AstroDom",
+    license="MIT",
+    author="Ferrante Enriques",
+    author_email="ferrante.enriques@gmail.com",
+    description="Astroimaging Catalogue Software",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: X11 Applications :: Qt",
+        "Environment :: Other Environment",
+        "Programming Language :: Python :: 3 :: Only",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: POSIX :: Linux",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+    ],
+)
